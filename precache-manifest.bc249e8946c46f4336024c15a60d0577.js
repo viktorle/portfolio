f977087837428950fe90ac1368ca54465d922137
viktorle <35763150+viktorle@users.noreply.github.com>
2020-04-20 15:38:27 +0200

    Updates

---
+++ precache-manifest.bc249e8946c46f4336024c15a60d0577.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc1c0a5f0b86b13af192d7bd7a9b2edf",
+    "url": "/portfolio/portfolio/index.html"
+  },
+  {
+    "revision": "690b886b85133ce99415",
+    "url": "/portfolio/portfolio/static/css/main.6a41bdf7.chunk.css"
+  },
+  {
+    "revision": "c266aced400cebd2cf4a",
+    "url": "/portfolio/portfolio/static/js/2.da0ac57d.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/portfolio/portfolio/static/js/2.da0ac57d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "690b886b85133ce99415",
+    "url": "/portfolio/portfolio/static/js/main.fd06f3cc.chunk.js"
+  },
+  {
+    "revision": "ca17db21ef23b4db168e",
+    "url": "/portfolio/portfolio/static/js/runtime-main.ccbeeb12.js"
+  },
+  {
+    "revision": "aad33909c8de23949c24e7ad52b4a465",
+    "url": "/portfolio/portfolio/static/media/Flyingbear.aad33909.jpg"
+  },
+  {
+    "revision": "db4de88d8e689ad12a10d9b958f63458",
+    "url": "/portfolio/portfolio/static/media/Viktor2.db4de88d.png"
+  },
+  {
+    "revision": "d72cc8978efede6bf9826e639ed0dcb6",
+    "url": "/portfolio/portfolio/static/media/aquaria.d72cc897.jpg"
+  },
+  {
+    "revision": "613f33d15fb4fc89caff0bdfa38492b2",
+    "url": "/portfolio/portfolio/static/media/carbCalc.613f33d1.png"
+  },
+  {
+    "revision": "180894a806b7df8fe8708f246f783ae8",
+    "url": "/portfolio/portfolio/static/media/chat.180894a8.png"
+  },
+  {
+    "revision": "6ae8894588d9df3b8f319c25adb8967d",
+    "url": "/portfolio/portfolio/static/media/css.6ae88945.png"
+  },
+  {
+    "revision": "27398d02f29b675367da839e33c9577b",
+    "url": "/portfolio/portfolio/static/media/flightViz.27398d02.png"
+  },
+  {
+    "revision": "adbdc925137661fc9b8a34dcc508a251",
+    "url": "/portfolio/portfolio/static/media/flutter.adbdc925.png"
+  },
+  {
+    "revision": "58a13d0eae9ee480e811753726911d72",
+    "url": "/portfolio/portfolio/static/media/gronaLund.58a13d0e.png"
+  },
+  {
+    "revision": "fd2f0ff5fb933fe7ac12faf53e539477",
+    "url": "/portfolio/portfolio/static/media/html.fd2f0ff5.png"
+  },
+  {
+    "revision": "efdba92da6f14e39a5db4df5b59e13b0",
+    "url": "/portfolio/portfolio/static/media/java.efdba92d.jpg"
+  },
+  {
+    "revision": "8b8524a46e91c819bc7a33d13c1daa6b",
+    "url": "/portfolio/portfolio/static/media/js.8b8524a4.png"
+  },
+  {
+    "revision": "f59f0f96637607eae90ba152632c88a5",
+    "url": "/portfolio/portfolio/static/media/kth.f59f0f96.png"
+  },
+  {
+    "revision": "5a305d7125e486422a74298e6de62408",
+    "url": "/portfolio/portfolio/static/media/matlab.5a305d71.png"
+  },
+  {
+    "revision": "39dff610661b307f38a28b07ff4c8ec0",
+    "url": "/portfolio/portfolio/static/media/php.39dff610.png"
+  },
+  {
+    "revision": "47ce6e77f039020ee2e76a10c1e988e9",
+    "url": "/portfolio/portfolio/static/media/react.47ce6e77.png"
+  },
+  {
+    "revision": "58c9046877a3650b317b85a392fcf6a8",
+    "url": "/portfolio/portfolio/static/media/skansen.58c90468.png"
+  },
+  {
+    "revision": "00ad3f097f10fb15eb2b73295754dbd8",
+    "url": "/portfolio/portfolio/static/media/sql.00ad3f09.jpeg"
+  },
+  {
+    "revision": "cf427b964140283bbebbc5a5a08f89f0",
+    "url": "/portfolio/portfolio/static/media/tue.cf427b96.png"
+  },
+  {
+    "revision": "015d555f5ac1467bb5411024938bc82b",
+    "url": "/portfolio/portfolio/static/media/unity.015d555f.jpg"
+  }
+]);
